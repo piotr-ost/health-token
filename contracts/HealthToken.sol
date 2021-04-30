f16d244f74ac8987030e46ed1c61a3c9f60c08d2
danush <danushkaadikari@gmail.com>
2021-04-30 19:10:11 +0700

    removes mint new token functions

--- contracts/HealthToken.sol
+++ contracts/HealthToken.sol
@@ -511,37 +511,6 @@ contract HealthToken is Context, IBEP20, Ownable {
   }
 
   /**
-   * @dev Creates `amount` tokens and assigns them to `msg.sender`, increasing
-   * the total supply.
-   *
-   * Requirements
-   *
-   * - `msg.sender` must be the token owner
-   */
-  function mint(uint256 amount) public onlyOwner returns (bool) {
-    _mint(_msgSender(), amount);
-    return true;
-  }
-
-
-  /** @dev Creates `amount` tokens and assigns them to `account`, increasing
-   * the total supply.
-   *
-   * Emits a {Transfer} event with `from` set to the zero address.
-   *
-   * Requirements
-   *
-   * - `to` cannot be the zero address.
-   */
-  function _mint(address account, uint256 amount) internal {
-    require(account != address(0), "BEP20: mint to the zero address");
-
-    _totalSupply = _totalSupply.add(amount);
-    _balances[account] = _balances[account].add(amount);
-    emit Transfer(address(0), account, amount);
-  }
-
-  /**
    * @dev Destroys `amount` tokens from `account`, reducing the
    * total supply.
    *
